0d9617b5abe4d47c4b1910e8446d0529618059b3
Seung Wan Yoo <74849806+wannieman98@users.noreply.github.com>
2021-07-12 16:09:30 +0900

    Update vocabs.py

--- vocabs.py
+++ vocabs.py
@@ -1,20 +1,21 @@
 import pickle
-import torchtext as tt
+import torchtext.vocab as tt
 
 UNK_IDX, PAD_IDX, SOS_IDX, EOS_IDX = 0, 1, 2, 3
 
 def build_vocabs(sentences, tokens):
-    vocabs = {}
+  vocabs = {}
+  
+  special_symbols =['<unk>', '<pad>', '<bos>', '<eos>']
 
-    special_symbols =['<unk>', '<pad>', '<bos>', '<eos>']
-
-    assert tt.__version__ != '0.10.0', "Need torchtext 0.10.0 <= in order to build vocabs."
-
-    for ln in ['src_lang', 'tgt_lang']:
-        vocabs[ln] = tt.build_vocab_from_iterator(iterator=yield_tokens(sentences, ln, tokens),
-                                                  min_freq=3,
-                                                  specials=special_symbols,
-                                                  special_first=True)
+  for ln in ['src_lang', 'tgt_lang']:
+      vocabs[ln] = tt.build_vocab_from_iterator(iterator=yield_tokens(sentences, ln, tokens),
+                                                min_freq=3,
+                                                specials=special_symbols,
+                                                special_first=True
+                                                )
+  return vocabs
+    
 
 def yield_tokens(data_iter, language, tokens):
     data_iter = data_iter[language]
