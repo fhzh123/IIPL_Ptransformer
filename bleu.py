476fefa664f3fb2620f874e896e1b8f83dbf4acb
jury124 <jury124@naver.com>
2021-07-22 12:43:23 +0900

    get bleu 수정

--- bleu.py
+++ bleu.py
@@ -8,10 +8,8 @@ from tqdm import tqdm
 import torch
 from torch.nn import functional as F
 
-def get_bleu(device,variation):
+def get_bleu(model,device,variation):
 
-    model = torch.load(f'./data/checkpoints/{variation}_checkpoint.pt')
-    
     chencherry = bs.SmoothingFunction()
     bleu_scores = 0
     src_list, tgt_list = [], []
